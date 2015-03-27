189fc64b1d28272fc7f90c1ffe2d507e47056275
RePod <repod@users.noreply.github.com>
2015-03-27 16:53:59 -0400

    Completely rewrite captcha.
    And hate every minute of it.

--- php_captcha.php
+++ php_captcha.php
@@ -1,26 +1,53 @@
 <?php
+//TO-DO: Not this, at all. Switch to recaptcha or something. - RePod.
+//Increasing difficulty for humans with no real stats for bots.
 session_start();
 
-$RandomStr = md5(microtime());// md5 to generate the random string
+//Configuration.
+$lines = rand(1,4); //Amount of lines to have.
+$padding = rand(1,8); //Initial horizontal offset.
+$length = 4; //Length of captcha. Does not prevent from overflowing.
 
-$ResultStr = substr($RandomStr,0,5);//trim 5 digit 
+function rx() {
+	return rand(0,100);
+}
+function ry() {
+	return rand(0,30);
+}
 
-$NewImage =imagecreatefromjpeg("img.jpg");//image create by existing image and as back ground 
 
-$LineColor = imagecolorallocate($NewImage,233,239,239);//line color 
-$TextColor = imagecolorallocate($NewImage, 255, 255, 255);//text color-white
+//Generate captcha and set in session.
+$start = rand(0,25);
+$captcha = strtoupper(substr(md5(microtime()),$start,$length));
+$_SESSION['capkey'] = $captcha;
 
-//imageline($NewImage,1,1,40,40,$LineColor);
-imageline($NewImage,1,100,50,0,$LineColor);
-imageline($NewImage,1,10,100,40,$LineColor);
-imageline($NewImage,30,0,120,50,$LineColor);
+//Create image.
+$image = @imagecreatetruecolor(100, 30) or die("Cannot Initialize new GD image stream");
 
-imagestring($NewImage, 5, 16, 4, $ResultStr, $TextColor);// Draw a random string horizontally 
+while ($lines) {
+	imagesetthickness($image, rand(1,2));
+	$line_color = imagecolorallocate($image,rand(153,255),rand(153,255),rand(153,255));
+	imageline($image,rx(),ry(),rx(),ry(),$line_color);
+	$lines--;
+}
 
-$_SESSION['capkey'] = $ResultStr;// carry the data through session
+//Write out each letter individually.
+$index = 0; $x = $padding;
+while ($index < strlen($captcha)) {
+	$text_color = imagecolorallocate($image, rand(153,255), rand(153,255), rand(153,255));
+	
+	//Calculate position.
+	$f = rand(3,5);
+	$x = $x + imagefontwidth($f) + rand(2,6);
+	$y = rand(4,(30 - imagefontheight($f) - 4));
+	imagestring($image, $f, $x+1, $y+1, substr($captcha,$index,1), $text_color);
+	imagestring($image, $f, $x, $y, substr($captcha,$index,1), $text_color);
+	$index++;
+}
 
-header("Content-type: image/jpeg");// out out the image 
 
-imagejpeg($NewImage);//Output image to browser 
+//Output image.
+header("Content-type: image/jpeg");
+imagejpeg($image);//Output image to browser 
 
 ?>
