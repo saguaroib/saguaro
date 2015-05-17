e95e46fd057fdc5e5ffd043bc34a6a5ce23672f2
RePod <repod@users.noreply.github.com>
2015-05-17 19:14:23 -0400

    Redundant language translations.
    Use en-us as a base then apply the configured language.
    
    Changed all language files from defines to vars, which are then defined
    in language.php.

---
+++ lang/language.php
@@ -0,0 +1,17 @@
+<?php
+    //Require base translation.
+    require("en-us.php");
+
+    //Get defined language and include it, overwriting en-us as needed.
+    include("../config.php");
+    if (LANGUAGE && LANGUAGE !== "en-us") {
+        include(LANGUAGE . ".php");
+    }
+    
+    //Define constants.
+    foreach (get_defined_vars() as $KEY=>$VALUE) {
+        if (substr($KEY,0,2) === "S_") {
+            define($KEY,$VALUE);
+        }
+    }
+?>
