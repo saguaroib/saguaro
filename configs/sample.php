a716869227cba4673a88391cdb323f3c32f6d9e2
Apogate <wgbdev91@gmail.com>
2015-05-19 23:22:13 -0400

    Update sample.php

--- configs/sample.php
+++ configs/sample.php
@@ -24,11 +24,8 @@ define(SHOWTITLETXT, '1');                              //Show TITLE at top (1: 
 define(SHOWTITLEIMG, '0');                              //Show image at top (0: no, 1: single, 2: rotating)
 define(TITLEIMG, '');                                   //Title image (point to php file if rotating)
 define(LANGUAGE, 'en-us');                              //Language file to use from "lang" folder.
-<<<<<<< HEAD
 define(DATE_FORMAT, 'm/d/y');                           //Formatting for the date in each post, see http://php.net/manual/en/function.date.php for different options
 
-=======
->>>>>>> origin/master
 //From here down all these settings are optional.
 //Extra settings - No need to change these for a basic installation, but you may want these options
 define(MAX_KB, '2048');                                 //Maximum upload size in KB
@@ -74,15 +71,10 @@ define(CSSFILE3, 'css/kusaba.css');                     //location of the third 
 define(STYLESHEET_3, 'Kusaba');                         //Name of the third stylesheet.
 define(CSSFILE4, 'css/monotone.css');                   //location of the fourth stylesheet.
 define(STYLESHEET_4, 'Monotone');                       //Name of the fourth stylesheet.
-<<<<<<< HEAD
 /*define(CSSFILE3, 'css/kusaba.css');						//location of the third stylesheet.
 define(STYLESHEET_3, 'Kusaba');						//Name of the third stylesheet.
 define(CSSFILE4, 'css/monotone.css');						//location of the fourth stylesheet.
 define(STYLESHEET_4, 'Monotone');						//Name of the fourth stylesheet.*/
-
-
-=======
->>>>>>> origin/master
 //Capcodes - show 'em who's boss (put it as your trip. IE: "name#CHANGEME" would result as "name## Admin ##!09EKYZv3TU")
 define("ADMIN_PASS", 'CHANGEME');                                   //admin pass
 define("ACAPCODE", ' <font color="FF101A"> ## Admin ## </font>');   //admin capcode
@@ -111,14 +103,9 @@ define(ADS1, '<center>ads ads ads</center>');   //advertisement code (top)
 define(USE_ADS2, 0);                            //Use advertisements (below post form) (1: yes  0: no)
 define(ADS2, '<center>ads ads ads</center>');   //advertisement code (below post form)
 define(USE_ADS3, 0);                            //Use advertisements (bottom) (1: yes  0: no)
-<<<<<<< HEAD
 define(ADS3, '<center>ads ads ads</center>');   //advertisement code (bottom)
 
 //BEWARE: Debug mode can display sensitive data that could be exploited. Use with caution
 define(DEBUG_MODE, 0);                              //0: off, 1: on. Enabling this will display any SQL errors as well as making redirects between posting/log updates slower.
 
 ?>
-=======
-define(ADS3, '<center>ads ads ads</center>');   //advertisement code (bottom)            
-?>
->>>>>>> origin/master
