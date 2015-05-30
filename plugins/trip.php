58a1167086ba568939ba1c9af6187d607f22507a
Apogate <wgbdev91@gmail.com>
2015-05-29 20:34:07 -0400

    Update trip.php

--- plugins/trip.php
+++ plugins/trip.php
@@ -3,32 +3,34 @@
 here and the other tripcodes script. PROTIP: i will leave you the last one (the other 2 came with this
 file), just go and try it out... you'll get a nice surprise xD
 
-	$trip=ereg_replace("TJ9qoWuqvA","<b><font color=#FF0000>0</font>ne</b>",$trip);
- 	$trip=ereg_replace("/.fjTeojqQ","Mr.Fluffy",$trip);
+$trip=ereg_replace("TJ9qoWuqvA","<b><font color=#FF0000>0</font>ne</b>",$trip);
+$trip=ereg_replace("/.fjTeojqQ","Mr.Fluffy",$trip);
 */
-	// If $cap is a staff password, use staff label
-	if ($regtrip == ADMIN_PASS)
-	{ $name	.= ACAPCODE; }
-	else if ($sectrip == ADMIN_PASS)
-	{ $name	.= ACAPCODE; }
-	else if ($sectrip == MOD_PASS)
-	{ $name	.= MCAPCODE; }
-	else if ($regtrip == MOD_PASS)
-	{ $name	.= MCAPCODE; }
-	else if ($regtrip == 'triforcetripomg')
-	{ $name     .= ' &nbsp;&nbsp;&#9650;&#11;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9650;&nbsp;&#9650; ';}
-	else if ($sectrip == 'triforcetripomg')
-	{ $name     .= ' &nbsp;&nbsp;&#9650;&#11;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9650;&nbsp;&#9650; ';}
+// If $cap is a staff password, use staff label
+if ( $regtrip == ADMIN_PASS ) {
+	$name .= ACAPCODE;
+} else if ( $sectrip == ADMIN_PASS ) {
+	$name .= ACAPCODE;
+} else if ( $sectrip == MOD_PASS ) {
+	$name .= MCAPCODE;
+} else if ( $regtrip == MOD_PASS ) {
+	$name .= MCAPCODE;
+} else if ( $regtrip == 'triforcetripomg' ) {
+	$name .= ' &nbsp;&nbsp;&#9650;&#11;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9650;&nbsp;&#9650; ';
+} /*else if ( $sectrip == 'triforcetripomg' ) {
+	$name .= ' &nbsp;&nbsp;&#9650;&#11;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9650;&nbsp;&#9650; ';
+}*/
 
 /*Setting up custom tripcodes
 
 uncomment lines below and change the text to the desired output. 
 The first changeme is what the tripcode you want to modify outputs. For example, a tripcode with the password #example outputs !6kgJ33pzx.
 You would paste 6kgJ33pzx (without the leading !) in the first changeme, and whatever you want to display in the second changeme
+*/
 
-
- 	$trip=ereg_replace("Ep8pui8Vw2","<font color=#FF0000>Raging Homo</font>",$trip);
- 	$trip=ereg_replace("Ep8pui8Vw2","<font color=#FF0000>Raging Homo</font>",$trip);
- 	$trip=ereg_replace("Ep8pui8Vw2","<font color=#FF0000>Raging Homo</font>",$trip);
+//$trip=ereg_replace("Ep8pui8Vw2","<font color=#FF0000>Raging Homo</font>",$trip);
+//$trip=ereg_replace("CHANGEME","CHANGEME2",$trip);
+//$trip=ereg_replace("CHANGEME","CHANGEME2",$trip);
+//$trip=ereg_replace("CHANGEME","CHANGEME2",$trip);
 
 ?>
