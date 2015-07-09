e518ce4b1fe7502d57708514dc9db9ff81497433
Apogate <wgbdev91@gmail.com>
2015-07-08 20:03:14 -0400

    #33 - Bans CSS

--- banned.php
+++ banned.php
@@ -103,7 +103,7 @@ if ($ip == $host) {
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><title>You ' . $status . '</title>
-<link href="css/banned.css" rel="stylesheet" type="text/css" />
+<link href="' . CSSPATH .'banned.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class="container">
