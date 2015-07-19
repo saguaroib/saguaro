a89abb61b212bd7b815ba381262e62d008a5ba66
RePod <repod@users.noreply.github.com>
2015-07-19 11:11:46 -0400

    Added MySQL check to test.php
    #68

--- test.php
+++ test.php
@@ -1,24 +1,43 @@
 <?php
+include("config.php");
+
 $min_php = '4.2.0';
 $min_gd = '2.0.0';
+$min_mysql = '4.0.0';
+
+
 
 $tests = [];
 
 //Return true if PHP is at or above $min_php, false otherwise.
-$tests["PHP Version"] =
+$tests["PHP version"] =
     [
         "current" => phpversion(),
         "valid" => version_compare(phpversion(), $min_php, '>='),
         "min" => $min_php
     ];
 
-//Return true if GD is at or above $min_gd, false otherwise.
-$tests["GD Version"] =
+$tests["GD version"] =
     [
         "current" => (function_exists("gd_info")) ? gd_info()["GD Version"] : 0,
         "valid" => (function_exists("gd_info")) ? version_compare(gd_info()["GD Version"], $min_gd, '>=') : 0,
         "min" => $min_gd
     ];
+
+$mysqli = new mysqli(SQLHOST, SQLUSER, SQLPASS);
+if (mysqli_connect_errno()) {
+    $tests["MySQL version"] = ["current" => 0, "valid" => 0, "min" => $min_mysql];
+} else {
+    $mver = mysqli_get_server_info($mysqli);
+    
+    $tests["MySQL version"] =
+        [
+            "current" => $mver,
+            "valid" => version_compare($mver, $min_mysql, '>='),
+            "min" => $min_mysql
+        ];
+}
+mysqli_close($mysqli);
 
 echo "Saguaro testing utility:<br><br>";
 
