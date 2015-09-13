1e13f4843a3d5f1a5d7ec3131acbd003564c735e
RePod <RePod@users.noreply.github.com>
2015-09-13 12:19:38 -0400

    Captcha - Revise comment section and line drawing.
    A majority of lines are now drawn below the text.

--- _core/general/captcha.php
+++ _core/general/captcha.php
@@ -5,10 +5,10 @@
     CAPTCHA.
 
     If accessed directly, the captcha image will be generated and stores the generated code in the session.
-    
+
     $captcha = new Captcha;
     if ($captcha->isValid()) { }
-    
+
     TO-DO: Not this, at all. Switch to recaptcha or something.
 
 */
@@ -17,61 +17,72 @@ class Captcha {
     function isValid() {
         return $this->validate();
     }
-    
+
     private function validate() {
         return (strtoupper($_REQUEST['num']) === $_SESSION['captcha_key']) ? true : false;
     }
-    
+
     function generate() {
         //Generate captcha and set in session.
         $start = rand(0,25);
         $length = 5; //Length of captcha. Does not prevent from overflowing.
         $captcha = strtoupper(substr(md5(microtime()),$start,$length));
-        
+
         session_start();
         $_SESSION['captcha_key'] = $captcha;
 
         //Configuration.
-        $lines = rand(2,4); //Amount of lines to have.
+        $lines = rand(2,4); //Amount of lines to draw.
+        $circles = rand(1,3); //Amount of circles to draw.
         $padding = rand(1,8); //Initial horizontal offset.
 
-        //Helper functions
-        function rx() {
-            return rand(0,100);
-        }
-        function ry() {
-            return rand(0,30);
-        }
+        //Helper functions.
+        function rx() { return rand(0,100); }
+        function ry() { return rand(0,30); }
 
         //Create image.
         $image = @imagecreatetruecolor(100, 30) or die("Cannot Initialize new GD image stream");
 
+        //Draw half the lines now.
+        $red = floor($lines * 0.75);
+        $lines -= $red;
+        $this->drawLines($image, $red);
+
         //Write out each letter individually.
         $index = 0; $x = $padding;
         while ($index < strlen($captcha)) {
-            $text_color = imagecolorallocate($image, rand(153,255), rand(153,255), rand(153,255));
             $bg_color = imagecolorallocate($image, rand(153,255), rand(153,255), rand(153,255));
-            
+            $text_color = imagecolorallocate($image, rand(153,255), rand(153,255), rand(153,255));
+
             //Calculate position.
             $f = rand(4,5);
-            $x = $x + imagefontwidth($f) + rand(2,6);
+            $x = $x + imagefontwidth($f) + rand(3,8);
             $y = rand(4,(30 - imagefontheight($f) - 4));
             imagestring($image, $f, $x+rand(-1,1), $y+rand(-1,1), substr($captcha,$index,1), $bg_color);
             imagestring($image, $f, $x, $y, substr($captcha,$index,1), $text_color);
             $index++;
         }
-        
-        while ($lines) {
-            imagesetthickness($image, rand(1,2));
-            $line_color = imagecolorallocate($image,rand(0,152),rand(0,152),rand(0,152));
-            imageline($image,rx(),ry(),rx(),ry(),$line_color);
-            $lines--;
-        }
 
+        //Draw the rest of the lines.
+        $this->drawLines($image, $lines);
+
+        //PHP >= 4.3
+        //$image = imagerotate($image, rand(-17,17), 0);
 
         //Output image.
         header("Content-type: image/jpeg");
-        imagejpeg($image);//Output image to browser 
+        imagejpeg($image);
+    }
+
+    private function drawLines($image, $amount) {
+        $amount = ($amount) ? $amount : 1;
+
+        while ($amount) {
+            imagesetthickness($image, rand(1,2));
+            $line_color = imagecolorallocate($image,rand(0,152),rand(0,152),rand(0,152));
+            imageline($image, rx(), ry(), rx(), ry(), $line_color);
+            $amount--;
+        }
     }
 }
 
