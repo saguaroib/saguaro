63ae9ca024ebf13900c15a7a9b880f1ab4209982
RePod <RePod@users.noreply.github.com>
2015-09-14 04:10:52 -0400

    BBCode - Convert commas in/out for additional options.
    Color processor now supports rgb(#,#,#).

--- _core/general/bbcode.php
+++ _core/general/bbcode.php
@@ -12,27 +12,26 @@
 
 class BBCode {
     public $processors = [
-        "/\[b\](.*)\[\/b\]/Usi" => "<span style='font-weight:bold'>\\1</span>", //Bold
-        "/\[u\](.*)\[\/u\]/Usi" => "<span style='text-decoration: underline'>\\1</span>", //Underline
-        "/\[i\](.*)\[\/i\]/Usi" => "<span style='font-style:italic'>\\1</span>", //Italic
-        "/\[spoiler\](.*)\[\/spoiler\]/Usi" => "<span class='spoiler'>\\1</span>", //Spoiler
-        "/\[color=(\#[0-9A-F]{6}|[a-z]+)\](.*)\[\/color\]/Usi" => "<span style='color:\\1'>\\2</span>", //Color
-        "/\[s\](.*)\[\/s\]/Usi" => "<span style='text-decoration: line-through'>\\1</span>", //Strikethrough.
-        "/\[size=([1-9])\d*?\](.*)\[\/size\]/Usi" => "<span style='font-size:\\1ex'>\\2</span>", //Size
-        "/\[aa\](.*)\[\/aa\]/Usi" => "<span style='font-family: Mona,\"MS PGothic\"'>\\1</span>", //ASCII Art
-        "/\[youtube\](.*)youtube.com\/watch\?v=(.*)\[\/youtube\]/Usi" => "<object width='425' height='344'><param name='movie' value='http://www.youtube.com/v/\\2&hl=de&fs=1\'</param><param name='allowFullScreen' value='true'></param><embed src='http://www.youtube.com/v/\\2&hl=de&fs=1' type='application/x-shockwave-flash' allowfullscreen='true' width='425' height='344'></embed></object>", //Youtube
-        "/\[nico\](.*)nicovideo.jp\/watch\/(.*)\[\/nico\]/Usi" => "<script src='http://ext.nicovideo.jp/thumb_watch/\\2\' width='255' height='255'></script>", //niconico
-        "/\[spoiler\](.*)\[\/spoiler\]/" => "<span class='spoiler'>\\1</span>" //Spoilers
+        "/\[b\](.*?)\[\/b\]/Usi" => "<span style='font-weight:bold'>\\1</span>", //Bold
+        "/\[u\](.*?)\[\/u\]/Usi" => "<span style='text-decoration: underline'>\\1</span>", //Underline
+        "/\[i\](.*?)\[\/i\]/Usi" => "<span style='font-style:italic'>\\1</span>", //Italic
+        "/\[color=(\#[0-9a-f]{3,6}|[a-z]+|rgb\([\d,]+\))\](.*?)\[\/color\]/Usi" => "<span style='color:\\1'>\\2</span>", //Color
+        "/\[s\](.*?)\[\/s\]/Usi" => "<span style='text-decoration: line-through'>\\1</span>", //Strikethrough.
+        "/\[size=([1-9])\d*?\](.*?)\[\/size\]/Usi" => "<span style='font-size:\\1ex'>\\2</span>", //Size
+        "/\[aa\](.*?)\[\/aa\]/Usi" => "<span style='font-family: Mona,\"MS PGothic\"'>\\1</span>", //ASCII Art
+        "/\[youtube\](.*?)youtube.com\/watch\?v=(.*)\[\/youtube\]/Usi" => "<object width='425' height='344'><param name='movie' value='http://www.youtube.com/v/\\2&hl=de&fs=1\'</param><param name='allowFullScreen' value='true'></param><embed src='http://www.youtube.com/v/\\2&hl=de&fs=1' type='application/x-shockwave-flash' allowfullscreen='true' width='425' height='344'></embed></object>", //Youtube
+        "/\[nico\](.*?)nicovideo.jp\/watch\/(.*?)\[\/nico\]/Usi" => "<script src='http://ext.nicovideo.jp/thumb_watch/\\2\' width='255' height='255'></script>", //niconico
+        "/\[spoiler\](.*?)\[\/spoiler\]/" => "<span class='spoiler'>\\1</span>" //Spoilers
     ];
 
     private function process($input) {
-        $out = $input;
+        $out = str_replace("&#44;", ",", $input);
 
         foreach ($this->processors as $regex => $resub) {
             $out = preg_replace($regex, $resub, $out);
         }
 
-        return $out;
+        return str_replace( ",", "&#44;", $out);
     }
 
     function format($input) {
