d737ce7635d954b1c9a7634de65cf2dc6c428b57
RePod <RePod@users.noreply.github.com>
2015-09-27 13:24:03 -0400

    Regist - Update WebM processor.
    Stricter detection of video stream.
    Now detects if there's an audio stream for future "no audio" setting.

--- _core/regist/process/video.php
+++ _core/regist/process/video.php
@@ -3,7 +3,7 @@
 /*
 
     Handless processing video for Regist.
-    
+
     Currently requires handlers to be in the path.
 
 */
@@ -11,8 +11,8 @@
 class VideoProcessor {
     function process($input) {
         $info = $this->check($input);
-        
-        if ($info['type'] !== 'video') {
+
+        if (!$info['has_video']) {
             error("\"$upfile_name\" is not a valid WebM.", $dest);
         } else {
             global $W, $H;
@@ -20,18 +20,18 @@ class VideoProcessor {
             $H = $info['height'];
         }
     }
-    
+
     private function check ($input) {
         if ('which avprobe' || 'where avprobe') { return $this->process_avprobe($input); }
         else if ('which ffprobe' || 'where ffprobe') { return $this->process_ffprobe($input); }
-        
+
         return 0;
     }
-    
+
     function process_avprobe($input) {
         exec("avprobe -version", $version, $aye);
         $version = explode(" ",$version[0])[1];
-        
+
         if (version_compare($version, '0.9', '>=')) {
             //At or above avprobe 0.9, which has extra options (specifically JSON).
             //Eventually.
@@ -43,15 +43,16 @@ class VideoProcessor {
             preg_match("/^width=(\d+)$/msi", $probe, $width);
             preg_match("/^height=(\d+)$/msi", $probe, $height);
             preg_match("/^duration=([\d\.]+)$/msi", $probe, $dur);
-            preg_match("/^codec_type=([a-z]+)$/msi", $probe, $codec_type);
-            
-            return ['width' => $width[1], 'height' => $height[1], 'duration' => round($dur[1],1), 'type' => $codec_type[1]];
+            $has_video = preg_match("/^codec_type=video$/msi", $probe);
+            $has_audio = preg_match("/^codec_type=audio$/msi", $probe);
+
+            return ['width' => $width[1], 'height' => $height[1], 'duration' => round($dur[1],1), 'has_video' => $has_video, 'has_audio' => $has_audio];
         }
     }
-    
+
     function process_ffprobe($input) {
         //exec("ffprobe -print_format json -show_format -show_streams $input", $out, $aye);
-        
+
         var_dump($out);
     }
 }
