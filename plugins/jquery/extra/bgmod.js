57b3253f004dffb1bcf2b27d58717962733a0354
RePod <RePod@users.noreply.github.com>
2015-09-29 21:32:21 -0400

    jQuery - bgmod save/load to/from localStorage.
    The technology is here.

--- plugins/jquery/extra/bgmod.js
+++ plugins/jquery/extra/bgmod.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
     $(".filesize").click(function(e) {
         e.preventDefault();
-        $("body").css({
-            "background": "#FFF url('" + $(this).find("a").attr('href') + "') center/cover no-repeat fixed"
-        });
+        bgmod_save($(this).find("a").attr('href'));
+        bgmod_set();
     });
+    
+    function bgmod_save(a) {
+        localStorage['bgmod_url'] = a;
+    }
+    
+    function bgmod_set() {
+        if (localStorage['bgmod_url']) {
+            $("body").css({
+                "background": "#FFF url('" + localStorage['bgmod_url'] + "') center/cover no-repeat fixed"
+            });
+        }
+    }
+    
+    bgmod_set();
 });
