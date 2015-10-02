2c64f20463406edd9ebd0229c600e4dc7806d2a5
RePod <RePod@users.noreply.github.com>
2015-10-02 13:36:04 -0400

    Resredir - Switch to MySQL class.
    Works with MySQL and MySQLi.

--- _core/general/resredir.php
+++ _core/general/resredir.php
@@ -1,18 +1,20 @@
 <?php
 
-global $my_log;
+global $my_log, $mysql;
 
 $res = (int) $res;
 
-if (!$redir = mysql_call("select no,resto from " . SQLLOG . " where no=" . $res)) {
+if (!$redir = $mysql->fetch_row("select no,resto from " . SQLLOG . " where no=" . $res)) {
     echo S_SQLFAIL;
 }
 
-list($no, $resto) = mysql_fetch_row($redir);
+list($no, $resto) = $redir;
 
 if (!$no) {
-    $maxq = mysql_call("select max(no) from " . SQLLOG . "");
-    list($max) = mysql_fetch_row($maxq);
+    global $mysql;
+    
+    $maxq = $mysql->query("select max(no) from " . SQLLOG);
+    list($max) = $mysql->fetch_row($maxq);
     if (!$max || ($res > $max))
         header("HTTP/1.0 404 Not Found");
     else // res < max, so it must be deleted!
