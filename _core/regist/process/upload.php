faebfda75a7799f169ec4c3ba880ce58a4c25458
RePod <RePod@users.noreply.github.com>
2015-10-02 17:43:41 -0400

    Regist - Start breaking down.

---
+++ _core/regist/process/upload.php
@@ -0,0 +1,52 @@
+<?php
+
+global $upfile, $upfile_name;
+
+//Captcha check
+if (BOTCHECK === true && !valid('moderator')) {
+    require_once(CORE_DIR . '/general/captcha.php');
+    $captcha = new Captcha;
+
+    if ($captcha->isValid() !== true)
+        error(S_CAPFAIL, $upfile);
+}
+
+//Uploaded file check
+if ($_FILES["upfile"]["error"] > 0) {
+    if ($_FILES["upfile"]["error"] == UPLOAD_ERR_INI_SIZE || $_FILES["upfile"]["error"] == UPLOAD_ERR_FORM_SIZE)
+        error(S_TOOBIG, $upfile);
+    if ($_FILES["upfile"]["error"] == UPLOAD_ERR_PARTIAL || $_FILES["upfile"]["error"] == UPLOAD_ERR_CANT_WRITE)
+        error(S_UPFAIL, $upfile);
+}
+
+if ($upfile_name && $_FILES["upfile"]["size"] == 0) {
+    error(S_TOOBIGORNONE, $upfile);
+}
+
+//Basic proxy check.
+if (PROXY_CHECK && preg_match("/^(mail|ns|dns|ftp|prox|pc|[^\.]\.[^\.]$)/", $host) > 0 || preg_match("/(ne|ad|bbtec|aol|uu|(asahi-net|rim)\.or)\.(com|net|jp)$/", $host) > 0) {
+    if (@fsockopen($_SERVER["REMOTE_ADDR"], 80, $a, $b, 2) == 1) {
+        error(S_PROXY80, $dest);
+    } elseif (@fsockopen($_SERVER["REMOTE_ADDR"], 8080, $a, $b, 2) == 1) {
+        error(S_PROXY8080, $dest);
+    }
+}
+
+//Check if user is banned
+require_once(CORE_DIR . "/admin/banish.php");
+$checkban = new Banish;
+if (!$checkban->checkBan($_SERVER["REMOTE_ADDR"]))
+    error(S_BADHOST, $upfile);
+
+//Check if replying to locked thread
+$resto = (int) $resto;
+if ($resto) {
+    global $mysql;
+    $resto = (int) $resto;
+    $result = $mysql->fetch_array("SELECT * FROM " . SQLLOG . " WHERE no=$resto");
+    if ($result["locked"] == '1' && !valid('moderator'))
+        error(S_THREADLOCKED, $upfile);
+     mysql_free_result($query);
+}
+
+?>
--- _core/regist/regist.php
+++ _core/regist/regist.php
@@ -6,57 +6,12 @@ Eventually rewrite this.
 
 */
 
-//Check prereq conditions for post processing
+
 
 $upfile_name = $_FILES["upfile"]["name"];
 $upfile = $_FILES["upfile"]["tmp_name"];
 
-//Captcha check
-if (BOTCHECK === true && !valid('moderator')) {
-    require_once(CORE_DIR . '/general/captcha.php');
-    $captcha = new Captcha;
-
-    if ($captcha->isValid() !== true)
-        error(S_CAPFAIL, $upfile);
-}
-
-//Uploaded file check
-if ($_FILES["upfile"]["error"] > 0) {
-    if ($_FILES["upfile"]["error"] == UPLOAD_ERR_INI_SIZE || $_FILES["upfile"]["error"] == UPLOAD_ERR_FORM_SIZE)
-        error(S_TOOBIG, $upfile);
-    if ($_FILES["upfile"]["error"] == UPLOAD_ERR_PARTIAL || $_FILES["upfile"]["error"] == UPLOAD_ERR_CANT_WRITE)
-        error(S_UPFAIL, $upfile);
-}
-
-if ($upfile_name && $_FILES["upfile"]["size"] == 0) {
-    error(S_TOOBIGORNONE, $upfile);
-}
-
-//Basic proxy check.
-if (PROXY_CHECK && preg_match("/^(mail|ns|dns|ftp|prox|pc|[^\.]\.[^\.]$)/", $host) > 0 || preg_match("/(ne|ad|bbtec|aol|uu|(asahi-net|rim)\.or)\.(com|net|jp)$/", $host) > 0) {
-    if (@fsockopen($_SERVER["REMOTE_ADDR"], 80, $a, $b, 2) == 1) {
-        error(S_PROXY80, $dest);
-    } elseif (@fsockopen($_SERVER["REMOTE_ADDR"], 8080, $a, $b, 2) == 1) {
-        error(S_PROXY8080, $dest);
-    }
-}
-
-//Check if user is banned
-require_once(CORE_DIR . "/admin/banish.php");
-$checkban = new Banish;
-if (!$checkban->checkBan($_SERVER["REMOTE_ADDR"]))
-    error(S_BADHOST, $upfile);
-
-//Check if replying to locked thread
-$resto = (int) $resto;
-if ($resto) {
-    global $mysql;
-    $resto = (int) $resto;
-    $result = $mysql->fetch_array("SELECT * FROM " . SQLLOG . " WHERE no=$resto");
-    if ($result["locked"] == '1' && !valid('moderator'))
-        error(S_THREADLOCKED, $upfile);
-     mysql_free_result($query);
-}
+require_once('process/upload.php'); //Check prereq conditions for post processing
 
 global $my_log, $mysql, $path, $badstring, $badfile, $badip, $pwdc, $textonly;
 require_once("cleanstr.php");
@@ -157,8 +112,6 @@ if ($has_image) {
     $mes = $upfile_name . ' ' . S_UPGOOD;
 }
 
-
-
 //The last result number
 $lastno = mysql_result($mysql->query("select max(no) from " . SQLLOG), 0, 0);
 
@@ -200,8 +153,7 @@ error(S_STRREF, $dest);
 }
 ;
 }*/
-if ($_SERVER["REQUEST_METHOD"] != "POST")
-    error(S_UNJUST, $dest);
+
 // Form content check
 if (!$name || preg_match("/^[ |&#12288;|]*$/", $name))
     $name = "";
