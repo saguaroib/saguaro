775eb12f18355e3b431c4dfa016c129c1f7576f1
RePod <RePod@users.noreply.github.com>
2015-10-27 08:33:52 -0400

    Head - Minor

--- _core/general/head.php
+++ _core/general/head.php
@@ -66,16 +66,16 @@ class Head {
                 <script src='" . JS_PATH . "/styleswitch.js' type='text/javascript'></script>
                 <script src='" . JS_PATH . "/main.js' type='text/javascript'></script>";
 
-        if (USE_JS_SETTINGS)       $dat .= '<script src="' . JS_PATH . '/suite_settings.js" type="text/javascript"></script>';
-        if (USE_IMG_HOVER)         $dat .= '<script src="' . JS_PATH . '/image_hover.js" type="text/javascript"></script>';
-        if (USE_IMG_TOOLBAR)     $dat .= '<script src="' . JS_PATH . '/image_toolbar.js" type="text/javascript"></script>';
-        if (USE_IMG_EXP)              $dat .= '<script src="' . JS_PATH . '/image_expansion.js" type="text/javascript"></script>';
-        if (USE_UTIL_QUOTE)        $dat .= '<script src="' . JS_PATH . '/utility_quotes.js" type="text/javascript"></script>';
-        if (USE_INF_SCROLL)        $dat .= '<script src="' . JS_PATH . '/infinite_scroll.js" type="text/javascript"></script>';
-        if (USE_FORCE_WRAP)    $dat .= '<script src="' . JS_PATH . '/force_post_wrap.js" type="text/javascript"></script>';
-        if (USE_UPDATER)            $dat .= '<script src="' . JS_PATH . '/thread_updater.js" type="text/javascript"></script>';
-        if (USE_THREAD_STATS)  $dat .= '<script src="' . JS_PATH . '/thread_stats.js" type="text/javascript"></script>';
-        if (REPOD_EXTRA)            $dat .= '<script src="' . JS_PATH . '/extra/bgmod.js" type="text/javascript"></script>';
+        if (USE_JS_SETTINGS)  $dat .= '<script src="' . JS_PATH . '/suite_settings.js" type="text/javascript"></script>';
+        if (USE_IMG_HOVER)    $dat .= '<script src="' . JS_PATH . '/image_hover.js" type="text/javascript"></script>';
+        if (USE_IMG_TOOLBAR)  $dat .= '<script src="' . JS_PATH . '/image_toolbar.js" type="text/javascript"></script>';
+        if (USE_IMG_EXP)      $dat .= '<script src="' . JS_PATH . '/image_expansion.js" type="text/javascript"></script>';
+        if (USE_UTIL_QUOTE)   $dat .= '<script src="' . JS_PATH . '/utility_quotes.js" type="text/javascript"></script>';
+        if (USE_INF_SCROLL)   $dat .= '<script src="' . JS_PATH . '/infinite_scroll.js" type="text/javascript"></script>';
+        if (USE_FORCE_WRAP)   $dat .= '<script src="' . JS_PATH . '/force_post_wrap.js" type="text/javascript"></script>';
+        if (USE_UPDATER)      $dat .= '<script src="' . JS_PATH . '/thread_updater.js" type="text/javascript"></script>';
+        if (USE_THREAD_STATS) $dat .= '<script src="' . JS_PATH . '/thread_stats.js" type="text/javascript"></script>';
+        if (REPOD_EXTRA)      $dat .= '<script src="' . JS_PATH . '/extra/bgmod.js" type="text/javascript"></script>';
         if (USE_EXTRAS) {
             foreach (glob(JS_PATH . "/extra/*.js") as $path) {
                 $dat .= "<script src='$path' type='text/javascript'></script>";
@@ -87,7 +87,7 @@ class Head {
                 <span class="boardList desktop">' . ((file_exists(BOARDLIST)) ? file_get_contents(BOARDLIST) : ''). '</div>
                 <div class="linkBar">[<a href="' . HOME . '" target="_top">' . S_HOME . '</a>][<a href="' . PHP_ASELF_ABS . '">' . S_ADMIN . '</a>]
                 </span><div class="boardBanner">' . $bannerImg . $boardTitle . '</div>' . $headSub . '
-                <a href="#top" /></a>';
+                <a href="#top"></a>';
 
         if (USE_ADS1) {
             $dat .= ADS1 . '<hr>';
