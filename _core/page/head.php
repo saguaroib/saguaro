6d69b6bac66431ab629cd2b1c86fe6734497fc33
Halcyon- <wgbdev91@gmail.com>
2015-11-09 00:25:47 -0500

    New admin head

--- _core/page/head.php
+++ _core/page/head.php
@@ -96,6 +96,74 @@ class Head {
 
         return $dat;
     }
+    
+    function generateAdmin() {
+        function generateAdmin() {
+        require_once(CORE_DIR . "/admin/report.php");
+
+        $getReport = new Report;
+        
+        $dat        = '';
+        $boardTitle = '';
+        $bannerImg  = '';
+        $headSub    = '';
+        
+        if (SHOWTITLETXT > 0) {
+            $boardTitle = "<div class='boardTitle'>" . TITLE . "</div>" . $headSub;
+            $headSub .= '<div class="boardSubtitle">' . S_HEADSUB . '</div><hr>';
+            if (SHOWTITLETXT == 2) //you cannot stop me repod i am invincible
+                $boardTitle = "<div class='boardTitle'/>/" . BOARD_DIR . "/ - " . TITLE . " Management</div>";
+        }
+        $bannerImg .= (SHOWTITLEIMG) ? '<img class="bannerImg" src="' . TITLEIMG . '" onclick="this.src=this.src;" alt="' . TITLE . '" /><br>' : '';
+        
+        /* begin page content */
+        $dat .= "<!DOCTYPE html><head>
+                    <meta name='description' content='" . S_DESCR . "'/></meta>
+                    <meta http-equiv='content-type'  content='text/html;charset=utf-8' /></meta>
+                    <meta name='viewport' content='width=device-width, initial-scale=1'></meta>
+                    <meta http-equiv='cache-control' content='max-age=0' />
+                    <meta http-equiv='cache-control' content='no-cache' />
+                    <meta http-equiv='expires' content='0' />
+                    <meta http-equiv='expires' content='Tue, 01 Jan 1980 1:00:00 GMT' />
+                    <meta http-equiv='pragma' content='no-cache' />
+                    <link rel='shortcut icon' href='" . CSS_PATH . "imgs/favicon.ico'>
+                    <title>" . $this->info['page']['title'] . "</title>";
+        
+        if (NSFW) {
+            $dat .= "<link class='togglesheet' rel='stylesheet' type='text/css' href='" . CSS_PATH . CSS1 . "' title='Saguaba' />
+                    <link class='togglesheet' rel='alternate stylesheet' type='text/css' media='screen'  href='" . CSS_PATH . CSS2 . "' title='Sagurichan' />";
+        } else {
+            $dat .= "<link class='togglesheet' rel='stylesheet' type='text/css' media='screen'  href='" . CSS_PATH . CSS2 . "' title='Sagurichan' />
+                <link class='togglesheet' rel='alternate stylesheet' type='text/css' href='" . CSS_PATH . CSS1 . "' title='Saguaba' />";
+        }
+        //<link class='togglesheet' rel='alternate stylesheet' type='text/css' media='screen'  href='" . CSS_PATH . CSS4 . "' title='Burichan'/> RIP Burichan 1862-2015
+        $dat .= "<link class='togglesheet' rel='alternate stylesheet' type='text/css' media='screen'  href='" . CSS_PATH . CSS3 . "' title='Tomorrow' />";
+        
+        $dat .= "<script src='" . JS_PATH . "/jquery.min.js' type='text/javascript'></script>
+                <script src='" . JS_PATH . "/styleswitch.js' type='text/javascript'></script>
+                <script src='" . JS_PATH . "/main.js' type='text/javascript'></script>";
+                
+        foreach ($this->info['css']['extra'] as $css) {
+            $dat .= "<link rel='stylesheet' type='text/css' href='" . CSS_PATH . "$css'/>";
+        }
+        
+        $dat .= EXTRA_SHIT . '</head><div class="beforePostform" />' . $titlebar . '
+                <span class="boardList desktop">' . ((file_exists(BOARDLIST)) ? file_get_contents(BOARDLIST) : '') . '</div>
+                <div class="linkBar">[<a href="' . HOME . '" target="_top">' . S_HOME . '</a>][<a href="' . PHP_ASELF_ABS . '">' . S_ADMIN . '</a>]
+                </span><div class="boardBanner">' . $bannerImg . $boardTitle . '</div>' . $headSub;            
+        
+        $dat .= "<div class='panelOps' style='text-align:left;' />[<a href=\"" . PHP_SELF2 . "\">" . S_RETURNS . "</a>]";
+        $dat .= "[<a href=\"" . PHP_SELF . "\">" . S_LOGUPD . "</a>]";
+        if (valid('moderator')) {
+            $dat .= "[<a href='" . PHP_ASELF_ABS . "?mode=rebuild' >Rebuild</a>]";
+            $dat .= "[<a href='" . PHP_ASELF_ABS . "?mode=rebuildall' >Rebuild all</a>]";
+            $dat .= "[<a href='" . PHP_ASELF_ABS . "?mode=reports' >" . $getReport->get_all_reports_board() . "</a>]";
+        }
+        if (valid('admin'))
+            $dat .= "[<a href='" . PHP_ASELF_ABS . "?mode=staff' >Manage users</a>]";
+        $dat .= "[<a href='" . PHP_ASELF . "?mode=logout'>" . S_LOGOUT . "</a>]";
+        return $dat;
+    } 
 }
 
 ?>
