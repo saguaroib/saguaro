b6b4d647443d35ea1db225beeb35d5ee10f6be95
RePod <RePod@users.noreply.github.com>
2015-11-10 17:58:59 -0500

    TextProcessor - Meme Arrows
    Currently doesn't work, blah blah, read the header comment.

---
+++ _core/general/text_process/memearrows.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+
+    Efficiency.
+
+    A lot stricter (and more accurate) than the old one due to caring about individual lines.
+    However, currently does not work due to usage of "<br />" without an accompanying "\n", see tests.
+    Regist would need to be changed to include these \n or run these before converting them to "<br />".
+    
+    Probably best to run after autolinker (which is currently impossible since autolinker is ran when posts are generated).
+
+    $test = new MemeArrows;
+    echo $test->format("test\n&gt;memearrows");       //Works because the \n splits correctly in the regex.
+    echo $test->format("test<br>\n&gt;memearrows");   //Works because the \n splits correctly in the regex.
+    echo $test->format("test<br />\n&gt;memearrows"); //Works because the \n splits correctly in the regex.
+    echo $test->format("test<br>&gt;memearrows");     //Doesn't work because PHP doesn't treat <br> as a newline to split by.
+    echo $test->format("test<br />&gt;memearrows");   //Doesn't work because PHP doesn't treat <br /> as a newline to split by.
+
+*/
+
+require_once("text_process.php");
+
+class MemeArrows extends TextProcessor {
+    public $processors = [
+        "/^(?:\&gt;|\>)(.*?)$/Umi" => "<span class='greentext' style='color:green'>&gt;\\1</span>"
+    ];
+}
+
+?>
