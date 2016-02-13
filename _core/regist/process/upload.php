255d42becc2c773caf86d0c78f88437d6cfd21bf
RePod <RePod@users.noreply.github.com>
2016-02-12 21:26:03 -0500

    Regist - Current progress update.
    Mostly changes to the upload processing classes to start returning
    information that will eventually be needed for the table insert.

--- _core/regist/process/upload.php
+++ _core/regist/process/upload.php
@@ -20,6 +20,7 @@ class UploadCheck {
         //These checks access the SQL server so we should prioritize these last and then order based on how intensive they are.
         if ($this->banned() !== true) error($this->last, $upfile); //Ban check.
         if ($this->locked() !== true) error($this->last, $upfile); //Lock check.
+        if ($this->media() !== true) error($this->last, $upfile); //Media check.
     }
 
     function captcha() {
@@ -36,12 +37,13 @@ class UploadCheck {
 
     function proxy() {
         //Basic proxy check.
+        $host = $_SERVER['REMOTE_ADDR'];
         if (PROXY_CHECK && preg_match("/^(mail|ns|dns|ftp|prox|pc|[^\.]\.[^\.]$)/", $host) > 0 || preg_match("/(ne|ad|bbtec|aol|uu|(asahi-net|rim)\.or)\.(com|net|jp)$/", $host) > 0) {
-            if (@fsockopen($_SERVER["REMOTE_ADDR"], 80, $a, $b, 2) == 1) {
+            if (@fsockopen($host, 80, $a, $b, 2) == 1) {
                 $this->last = S_PROXY80;
                 //error(S_PROXY80, $dest);
                 return false;
-            } elseif (@fsockopen($_SERVER["REMOTE_ADDR"], 8080, $a, $b, 2) == 1) {
+            } elseif (@fsockopen($host, 8080, $a, $b, 2) == 1) {
                 $this->last = S_PROXY8080;
                 //error(S_PROXY8080, $dest);
                 return false;
@@ -93,6 +95,25 @@ class UploadCheck {
             if ($result["locked"] == '1' && !valid('moderator')) {
                 $this->last = S_THREADLOCKED;
                 //error(S_THREADLOCKED, $upfile);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function media() {
+        $resto = (int) $resto;
+        if ($resto) {
+            global $mysql;
+            if (!$result = $mysql->query("select COUNT(*) from " . SQLLOG . " where resto=$resto and fsize!=0")) {
+                echo S_SQLFAIL;
+            }
+
+            $countimgres = $mysql->result($result, 0, 0);
+            if ($countimgres > MAX_IMGRES) {
+                $mysql->free_result($result);
+                $this->last = 'Media bump limit reached.';
+                //error("Max limit of " . MAX_IMGRES . " image replies has been reached.", $upfile);
                 return false;
             }
         }
--- _core/regist/process/upload_file.php
+++ _core/regist/process/upload_file.php
@@ -1,66 +1,69 @@
 <?php
 
-$has_image = $upfile && file_exists($upfile);
+/*
 
-if ($has_image) {
-    // check image limit
-    if ($resto) {
-        if (!$result = $mysql->query("select COUNT(*) from " . SQLLOG . " where resto=$resto and fsize!=0")) {
-            echo S_SQLFAIL;
-        }
+    ProcessFile for Regist
 
-        $countimgres = $mysql->result($result, 0, 0);
-        if ($countimgres > MAX_IMGRES)
-            error("Max limit of " . MAX_IMGRES . " image replies has been reached.", $upfile);
-        $mysql->free_result($result);
-    }
+    Extracts information from the desired file.
 
-    //upload processing
-    $dest = tempnam(substr($path, 0, -1), "img");
-    //$dest = $path.$tim.'.tmp';
-    if (OEKAKI_BOARD == 1 && $_POST['oe_chk']) {
-        rename($upfile, $dest);
-        chmod($dest, 0644);
-        if ($pchfile)
-            rename($pchfile, "$dest.pch");
-    } else
+*/
+
+class ProcessFile {
+    function run($upfile,$tim) {
+        global $path;
+        $info = [];
+
+        //upload processing
+        $extension = pathinfo($_FILES["upfile"]["name"], PATHINFO_EXTENSION); //Basic file extension, up for improvements.
+        $dest = $path . "$tim.$extension"; //Just name it right the first time.
+        //$dest = $path.$tim.'.tmp';
+        /*if (OEKAKI_BOARD == 1 && $_POST['oe_chk']) {
+            rename($upfile, $dest);
+            chmod($dest, 0644);
+            if ($pchfile)
+                rename($pchfile, "$dest.pch");
+        } else*/
+
         move_uploaded_file($upfile, $dest);
+        clearstatcache(); // otherwise $dest looks like 0 bytes!
 
-    clearstatcache(); // otherwise $dest looks like 0 bytes!
+        //$upfile_name = $sanitize->CleanStr($upfile_name, 0);
+        $fsize = filesize($dest);
 
-    $upfile_name = $sanitize->CleanStr($upfile_name, 0);
-    $fsize       = filesize($dest);
-
-    if (!is_file($dest))
-        error(S_UPFAIL, $dest);
-    if (!$fsize /*|| /*$fsize > MAX_KB * 1024*/)
-        error(S_TOOBIG, $dest);
-
-    preg_match('/(\.\w+)$/', $upfile_name, $ext);
-    $ext = $ext[0]; //Obtain extension.
-
-    // PDF processing
-    if (ENABLE_PDF == 1 && strcasecmp('.pdf', substr($upfile_name, -4)) == 0) {
-        $ext = '.pdf';
-        $W   = $H = 1;
-        $md5 = md5_file($dest);
-        // run through ghostscript to check for validity
-        if (pclose(popen("/usr/local/bin/gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=nullpage $dest", 'w'))) {
+        if (!is_file($dest))
             error(S_UPFAIL, $dest);
-        }
-    } else if ($ext == ".webm") {
-        global $W, $H;
-        require('video.php');
-        $processor = new VideoProcessor;
-        $processor->process($dest);
-    } else {
-        $maxw = (!$resto) ? MAX_W : MAXR_W;
-        $maxh = (!$resto) ? MAX_H : MAXR_H;
-        require_once("image.php");
-    }
+        if (!$fsize /*|| /*$fsize > MAX_KB * 1024*/)
+            error(S_TOOBIG, $dest);
 
-    $md5 = md5_file($dest);
-    $mes = $upfile_name . ' ' . S_UPGOOD;
+        // PDF processing
+        if (ENABLE_PDF == 1 && strcasecmp('.pdf', substr($upfile_name, -4)) == 0) {
+            $ext = '.pdf';
+            $W   = $H = 1;
+            // run through ghostscript to check for validity
+            if (pclose(popen("/usr/local/bin/gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=nullpage $dest", 'w'))) {
+                error(S_UPFAIL, $dest);
+            }
+        } else if ($extension == "webm") {
+            require('video.php');
+            $processor = new VideoProcessor;
+            $info = $processor->process($dest);
+        } else {
+            require_once('image.php');
+            $process = new ProcessImage;
+            $info = $process->run($dest);
+        }
+
+        $post = [
+            'md5' => md5_file($dest),
+            'filesize' => $fsize,
+            'original_name' => $_FILES["upfile"]["name"],
+            'original_extension' => $extension
+        ];
+        $info = array_merge($info,$post);
+        //$mes = $upfile_name . ' ' . S_UPGOOD;
+
+        return $info;
+    }
 }
 
 ?>
