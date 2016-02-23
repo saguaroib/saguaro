f67cb88cb447eec74b4be0ddec8d39f463c24b0e
john madden <wgbdev91@gmail.com>
2016-02-22 21:47:09 -0500

    Regist - DICE_ROLL, Thread IDs, minor reorganizing
    This adds a new feature for games, dice rolling.
    type your roll in the subject field like so, dice+2d6 and receive your
    roll in your post.
    
    Re-enables IDs, however they are in legacy mode for now until a better
    method of calculation is sorted out.

--- _core/regist/addons.php
+++ _core/regist/addons.php
@@ -1,13 +1,57 @@
 <?php
 
-function parseAddons($post) {
+function parseComment($com, $email) {
+	//Parses comments for special features. Accepts email for #triggering some actions
 	
+	$com = dice($com, $email); //Dice roll
 	
+	return $com;
 }
 
-function tripcodeify($name) {
-	
-	
+function userID($now) {
+	if(DISP_ID == true){
+		if($email)
+			$id .= " (ID: Heaven)";
+		else
+			$id.=" (ID:".substr(crypt(md5($_SERVER["REMOTE_ADDR"].'id'.date("Ymd", $time)),'id'),+3) . ")";
+		//Leave this escaped for storage
+		$now .= "<span class=\"posteruid\" id=\"posterid\" style=\"border-radius:10px;font-size:8pt;\" />" . $id . "</span>";
+	}
+	return $now;
+}
+
+function dice($com, $email) {
+	if(DICE_ROLL) {
+		if ($email) {
+			if (preg_match("/dice[ +](\\d+)[ d+](\\d+)(([ +-]+?)(-?\\d+))?/", $email, $match)) {
+				$dicetxt = "rolled ";
+				$dicenum = min(25, $match[1]);
+				$diceside = $match[2];
+				$diceaddexpr = $match[3];
+				$dicesign = $match[4];
+				$diceadd = intval($match[5]);
+				
+				for ($i = 0; $i < $dicenum; $i++) {
+					$dicerand = mt_rand(1, $diceside);
+					if ($i) $dicetxt .= ", ";
+					$dicetxt .= $dicerand;
+					$dicesum += $dicerand;
+				}
+				
+				if ($diceaddexpr) {
+					if (strpos($dicesign, "-") > 0) $diceadd *= -1;
+					$dicetxt .= ($diceadd >= 0 ? " + " : " - ").abs($diceadd);
+					$dicesum += $diceadd;
+				}
+				
+				$dicetxt .= " = $dicesum<br /><br />";
+				$com = "<b>$dicetxt</b>" . $com;
+				
+				return $com;
+			}
+		}
+	}
+	return $com;
 }
 
 ?>
