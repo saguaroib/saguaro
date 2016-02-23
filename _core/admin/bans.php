78c900f249a6a0e5d7aaac56bfda517279b22f31
Apogate <wgbdev91@gmail.com>
2016-02-23 16:17:13 -0500

    Bans - Initial commit
    - New autoban function. Unimplemented for now.
    - Optimizing ban process
    - Cleaning up **admin.php**
    
    checklist with PR goals to follow

--- _core/admin/bans.php
+++ _core/admin/bans.php
@@ -1,61 +1,112 @@
 <?php
+/*
+
+    ==================================== Saguaro Ban Class ========================================
+    
+        This handles all the fun things ban related.
+        Stores ip information 3 different ways now. Absolutely incredible.
+        
+        Check if a user is banned. If they are, the function will return true. Call with the "redirect" flag to send the user to banned.php.
+        require_once(CORE_DIR . "/admin/bans.php);
+        $bans = new Banish;
+        $bans->isBanned($ip, true); 
+    
+    ==========================================================================================
+    
+*/
+
+
 
 class Banish {
-	
-    function isBanned($ip) {
-		global $mysql;
+    
+    //If user is banned, return true.
+    function isBanned($ip, $redirect = 0) {
+        global $mysql;
+        
+        $query   = $mysql->query("SELECT count(*)>0 FROM " . SQLBANLOG . " WHERE host='$ip' AND active=1 AND (board='$board' or global=1)");
+        $exists = $mysql->result($query, 0, 0);
 
-		$query = "SELECT ip FROM " . SQLBANLOG . " WHERE ip='" . $mysql->escape_string($ip) . "' AND active>0";
-		$result = $mysql->num_rows($query);
+        $respond = ($redirect) ? header("Location: banned.php") : true; //If isbanned is called with redirect, then return the new header
+        return ($exists > 0) ? $respond : false;
+    }
 
-		return ($result > 0) ? true : false;
+
+    function process($no, $ip, $length, $global, $reason, $pubreason) {
+        global $mysql, $my_log;
+
+        //Append public ban message
+        $mysql->query("UPDATE " . SQLLOG . " SET com = CONCAT(com, '<br><b><font color=\"FF101A\">" . $mysql->escape_string($custmess) . "</font></b>') where no='" . $no . "'");
+
+        $my_log->update($no);
+        
+        echo "<script>window.close();</script>"; //Close ban window
+        
+        return true; //Success
     }
     
-	function postOptions($no, $ip, $expires, $expires2, $banType, $perma, $pubreason, $staffnote, $custmess, $showbanmess, $afterban) {
-        global $mysql, $my_log;
-		//This will do the POST processing and pass it to applyBan
-		
-		$str = "+" . $expires . " " . $expires2;
-		$expires = strtotime($str, time() );
-		
-        $custmess = ($showbanmess) ? ($custmess == '') ? "(USER WAS BANNED FOR THIS POST)" : "(" . $custmess . ")" : 0; //pls ignore
+    function autoBan($name, $host, $length, $global, $reason, $pubreason = '') {
+        global $mysql;
+        
+        //Get all IP info for the ban
+        $reverse = $mysql->escape_string(gethostbyaddr($host));
+        $xff     = $mysql->escape_string(getenv("HTTP_X_FORWARDED_FOR"));
+        $host    = $mysql->escape_string($host); //Proceed with tidy host
+        
+        //Already banned, don't insert again
+        if ($this->isBanned($host)) { 
+            Delete::deleteUploaded();
+            die();
+        }
+        
+        if (!$name) $name = S_ANONAME;
+        
+        if (strpos($name, '</span> <span class="postertrip">!') !== FALSE) {
+            $nameparts = explode('</span> <span class="postertrip">!', $name);
+            $name  = "{$nameparts[0]} #{$nameparts[1]}";
+        }        
+        
+        $name  = $mysql->escape_string($name);
+        $global    = ($global) ? 1 : 0;
+        $board     = BOARD_DIR;
+        $reason    = $mysql->escape_string($reason);
+        $pubreason = $mysql->escape_string($pubreason);
 
-        $afterban = (int) $afterban;
-		if ($afterban > 0) {
-            require_once(CORE_DIR . '/admin/delete.php');
-            $del = new Delete;
-			if ($afterban == 1):
-                $del->targeted($no, $pwd, $imgonly = 0, $automatic = 1, $children = 1, $die = 1);
-			elseif ($afterban == 2):
-                $del->targeted($no, $pwd, $imgonly = 1, $automatic = 1, $children = 1, $die = 1);
-			else:
-                $del->targeted($no, $pwd, $imgonly = 0, $automatic = 1, $children = 0, $die = 1, $ip);
-            endif;
-		}
-		
-		$mysql->query( "INSERT INTO " . SQLBANLOG . " (ip, active, placedon, expires, board, type, reason, staffnotes) 
-		VALUES ('" . $mysql->escape_string( $ip ) . "', 
-		'1', 
-		UNIX_TIMESTAMP(),
-		'" . $mysql->escape_string( $expires ) . "',
-		'" . BOARD_DIR . "', 
-		'" . $mysql->escape_string( $banType ) . "', 
-		'" . $mysql->escape_string( $pubreason ) . "', 
-		'" . $mysql->escape_string( $staffnote ) . "' )");
+        if ($pubreason) $pubreason .= "<>";
 
-		if ($custmess)
-			$mysql->query( "UPDATE " . SQLLOG . " SET com = CONCAT(com, '<br><b><font color=\"FF101A\">" . $mysql->escape_string( $custmess ) . "</font></b>') where no='" . $no . "'");  
-	
-        $my_log->update($no);
+        switch($banlength) {
+            case 0:
+                //Auto-warn
+                $autowarnq     = $mysql->query("SELECT COUNT(*) FROM " . SQLLOGBAN . " WHERE host='$host' AND admin='Auto-ban' AND now > DATE_SUB(NOW(),INTERVAL 3 DAY) AND reason like '%$reason'");
+                $autowarncount = $mysql->result($autowarnq, 0, 0);
+                if ($autowarncount > 3) $banlength = 14; //14 days
+                break;
+            case -1:
+                //Permanent
+                $length = '0000' . '00' . '00'; // YYYY/MM/DD
+                break;
+            default:
+                //Normal ban
+                $banlength = (int) $banlength;
+                $length = date("Ymd", time() + $banlength * (24 * 60 * 60));
+                break;
+        }
+
+        $length .= "00" . "00" . "00"; // H:M:S
+
+        if (!$result = $mysql->query("INSERT INTO " . SQLLOGBAN . " (board,global,name,host,reason,length,admin,reverse,xff) VALUES('$board','$global','$name','$host','$pubreason<b>Auto-ban</b>: $reason','$length','Auto-ban','$reverse','$xff')"))
+            echo S_SQLFAIL;
+            
+        @$mysql->free_result($result);
     }
-	
+    
+    //Ban filing form.
     function form($no) {
         global $mysql;
-    
-        $host = $mysql->result("SELECT host FROM " . SQLLOG . " WHERE no='" . $mysql->escape_string($no) . "'", 0, 0);
-        $alart = ($host) ? $mysql->num_rows("SELECT COUNT(*) FROM " . SQLBANLOG . " WHERE ip='" . $host . "'") : 0;
-        $alert = ( $alart > 0) ? "<b><font color=\"FF101A\"> $alart ban(s) on record for $host!</font></b>" : "No bans on record for IP $host";
-
+        
+        $host  = $mysql->result("SELECT host FROM " . SQLLOG . " WHERE no='" . $mysql->escape_string($no) . "'", 0, 0);
+        $alart = ($host) ? $mysql->result("SELECT COUNT(*) FROM " . SQLBANLOG . " WHERE ip='" . $host) : 0;
+        $alert = ($alart > 0) ? "<b><font color=\"FF101A\"> $alart ban(s) on record for $host!</font></b>" : "No bans on record for IP $host";
+        
         $temp = head(1);
         
         $temp .= "<!---banning #:$no; host:$host---><br><table border='0' cellpadding='0' cellspacing='0' /><form action='admin.php?mode=ban' method='POST' />
@@ -71,7 +122,7 @@ class Banish {
                 </select></td></tr>
             <center><tr><td class='postblock'>Ban type:</td><td></center>
                 <select name='banType' />
-                <option value='1' />Warning only</option>
+                <option value='0' />Warning only</option>
                 <option value='2' />This board - /" . BOARD_DIR . "/ </option>
                 <option value='3' />All boards</option>
                 <option value='4' />Permanent - All boards</option>
@@ -88,19 +139,14 @@ class Banish {
                 <option value='3' />Delete all by this IP</option>
                 </select>
             </td></tr>";
-            /*if (valid('admin'))
-                $temp .= "
-                <tr><td class='postblock'>Add to Blacklist:</td><td>[ Comment<input type='checkbox' name='blacklistcom' /> ] [ Image MD5<input type='checkbox' name='blacklistimage' /> ] </td></tr>";*/ //Soon.
-            $temp .= "<center><tr><td><input type='submit' value='Ban'/></td></tr></center></table></form>";
-            
-            echo $temp;
+        /*if (valid('admin'))
+        $temp .= "
+        <tr><td class='postblock'>Add to Blacklist:</td><td>[ Comment<input type='checkbox' name='blacklistcom' /> ] [ Image MD5<input type='checkbox' name='blacklistimage' /> ] </td></tr>";*/ //Soon.
+        $temp .= "<center><tr><td><input type='submit' value='Ban'/></td></tr></center></table></form>";
+        
+        echo $temp;
     }
     
-	function afterBan() {
-		//Display information for ban just executed, allow revert option, redirect to admin.
-		echo "Banned IP: " . $ip . ", Redirecting. <META HTTP-EQUIV=\"refresh\" content=\"2;URL=" . PHP_ASELF_ABS . "\">";
-	}
-	
 }
 
 ?>
