5c21bcf8870811060709798231956712894406f7
yolo "the merger" mcswaggins <wgbdev91@gmail.com>
2017-02-09 17:51:06 -0500

    Finish clear.php

--- _core/admin/reports/clear.php
+++ _core/admin/reports/clear.php
@@ -2,12 +2,10 @@
 
 
 class SaguaroClearReport {
+    public $message;
+
     public function clear() {
         global $mysql;
-
-        if ($_SERVER['REQUEST_METHOD'] != 'POST') }
-            return json_encode(['res'=> 'error', 'mes'=> 'Invalid request']);
-        }
 
         $post = [
             'no'    => (int) $_POST['no'],
@@ -15,8 +13,28 @@ class SaguaroClearReport {
             'csrf'  => $_POST['token']
         ];
 
-        //$mysql->result("SELECT COUNT(no) FROM " . SQLREPORTS . " WHERE active='1' AND no='{$post['no']}' AND board='{$post['board']}'");
-        
+        if ($_SERVER['REQUEST_METHOD'] != 'POST' || !valid('report', $post['board'])) {
+            $this->notify("error", "Invalid request");
+        }
 
+        $count = (int) $mysql->result("SELECT COUNT(no) FROM " . SQLREPORTS . " WHERE active='1' AND no='{$post['no']}' AND board='{$post['board']}'");
+        if ($count < 1) {
+            $this->notify("error", "Report has already been cleared or deleted");
+        }
+
+        $mysql->query("UPDATE " . SQLREPORTS . " SET active='0' WHERE no='{$post['no']}' AND board='{$post['board']}'");
+        $this->notify("good", "cleared");
+
+    }
+
+    private function notify($status = "good", $message = '') {
+        $ret = [
+            'res' => $status, 
+            'mes' => $message 
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($ret);
+        exit();
     }
 }
