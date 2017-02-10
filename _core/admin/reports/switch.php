7ba7ba344c4cee53df5f59526a4d1ed4db3043f0
Apogate <wgbdev91@gmail.com>
2017-02-09 19:13:49 -0500

    Final SQL, backend done

--- _core/admin/reports/switch.php
+++ _core/admin/reports/switch.php
@@ -5,6 +5,11 @@ switch($_GET['report']) {
         $rep = new SaguaroRQData;
         $rep->queueJSON();
         break;
+    case 'clear':
+        require_once(CORE_DIR . "/admin/reports/clear.php");
+        $clear = new SaguaroClearReport;
+        $clear->clear();
+        break;
 	default:
 		require_once(CORE_DIR . "/admin/reports/report.php");
         $report = new SaguaroReports;
