903a66054394056ab0418065f0658c594ec8d5ee
yolo "the merger" mcswaggins <wgbdev91@gmail.com>
2017-02-13 16:23:30 -0500

    wew

--- config.php
+++ config.php
@@ -169,7 +169,7 @@ define('PHP_SELF', 'imgboard.php');   //Name of main script file
 define('PHP_SELF2', 'index'.PHP_EXT); //Name of main htm file
 define('PHP_ASELF', 'admin.php');    // Name of Admin file
 define('PHP_ASELF_ABS', '//'.SITE_ROOT.'/'.BOARD_DIR.'/'.PHP_ASELF); //Path to admin file
-define('SITE_ROOT_BD', SITE_ROOT.'/'.BOARD_DIR);
+define('SITE_ROOT_BD', '//' . SITE_ROOT.'/'.BOARD_DIR);
 define('PHP_SELF_ABS', '//'.SITE_ROOT_BD.'/'.PHP_SELF);   // Absolute path from the site to the imgboard.php, ex: http://yoursite.com/boardDir/imgboard.php
 define('PHP_SELF2_ABS', '//'.SITE_ROOT_BD.'/'.PHP_SELF2); // Absolute path from the site to the INDEX.html, ex: http://yoursite.com/boardDir/index.html
 define('DATA_SERVER', '//'.SITE_ROOT.'/');                //Your site's root html path, WITH a trailing slash, ex: http://yoursite.com/
